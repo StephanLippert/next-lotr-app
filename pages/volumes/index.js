f1bd0e34b3d7ca8eb81fa4f6c3fe2e37d27973c9
Stephan Lippert <stephman1984@gmail.com>
2023-09-04 11:00:25 +0200

    little change

--- pages/volumes/index.js
+++ pages/volumes/index.js
@@ -23,7 +23,7 @@ export default function Volumes() {
           </Link>
         ))}
       </ul>
-      <button onClick={handleClick}>Click me tender</button>
+      <button onClick={handleClick}>Click me tender for a random Book</button>
     </>
   );
 }
